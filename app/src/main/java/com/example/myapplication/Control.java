a60cf0c31a52775590ba47bb7fee2f759d407288
jeremyglam <52576926+jeremyglam@users.noreply.github.com>
2019-07-27 00:06:05 +0800

    Update Control.java

--- app/src/main/java/com/example/myapplication/Control.java
+++ app/src/main/java/com/example/myapplication/Control.java
@@ -268,6 +268,7 @@ public class Control extends Activity {
 			// TODO Auto-generated method stub
 			if(value != null){
 				tv_currentRSSI.setText(value);
+
 			}
 		}
 
@@ -298,6 +299,20 @@ public class Control extends Activity {
 		}.start();
 	}
 
+	protected static double calculateAccuracy(int txPower, double rssi) {
+		if (rssi == 0) {
+			return -1.0; // if we cannot determine accuracy, return -1.
+		}
+
+		double ratio = rssi*1.0/txPower;
+		if (ratio < 1.0) {
+			return Math.pow(ratio,10);
+		}
+		else {
+			double accuracy =  (0.89976)*Math.pow(ratio,7.7095) + 0.111;
+			return accuracy;
+		}
+	}
 
 	private void displayData(byte[] data) {
 		if (data != null) {
@@ -365,7 +380,6 @@ public class Control extends Activity {
 		 
 		 ArrayList<ArrayList<HashMap<String, String>>> gattCharacteristicData
                 = new ArrayList<ArrayList<HashMap<String, String>>>();
-        
 		 mGattCharacteristics = new ArrayList<ArrayList<BluetoothGattCharacteristic>>();
         
 		 // Loops through available GATT Services.
